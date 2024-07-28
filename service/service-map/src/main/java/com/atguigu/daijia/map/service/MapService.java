46208236aba1b29d12e5c2005271439feeb88712
Nico-human <alandong4748@gmail.com>
2024-07-28 19:06:41 +0800

    Feature 1.1 乘客端订单价格预估

--- service/service-map/src/main/java/com/atguigu/daijia/map/service/MapService.java
+++ service/service-map/src/main/java/com/atguigu/daijia/map/service/MapService.java
@@ -1,5 +1,16 @@
 package com.atguigu.daijia.map.service;
 
+import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
+import com.atguigu.daijia.model.vo.map.DrivingLineVo;
+
 public interface MapService {
 
+    /**
+     * 请求腾讯地图服务得到预估路线, 距离, 时间
+     * TODO: 需要先去腾讯位置服务 <a href="https://lbs.qq.com/dev/console/quota/account">...</a> 分配额度
+     * 给 周边推荐(explore), 关键词输入提示, 驾车路线规划 三个接口分配额度, 否则无法使用
+     * @param calculateDrivingLineForm
+     * @return
+     */
+    DrivingLineVo calculateDrivingLine(CalculateDrivingLineForm calculateDrivingLineForm);
 }
--- web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/impl/OrderServiceImpl.java
@@ -1,12 +1,66 @@
 package com.atguigu.daijia.customer.service.impl;
 
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
 import com.atguigu.daijia.customer.service.OrderService;
+import com.atguigu.daijia.map.client.MapFeignClient;
+import com.atguigu.daijia.model.form.customer.ExpectOrderForm;
+import com.atguigu.daijia.model.form.map.CalculateDrivingLineForm;
+import com.atguigu.daijia.model.form.rules.FeeRuleRequestForm;
+import com.atguigu.daijia.model.vo.customer.ExpectOrderVo;
+import com.atguigu.daijia.model.vo.map.DrivingLineVo;
+import com.atguigu.daijia.model.vo.rules.FeeRuleResponseVo;
+import com.atguigu.daijia.rules.client.FeeRuleFeignClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Slf4j
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderServiceImpl implements OrderService {
 
+    @Autowired
+    private MapFeignClient mapFeignClient;
+
+    @Autowired
+    private FeeRuleFeignClient feeRuleFeignClient;
+
+    @Override
+    public ExpectOrderVo expectOrder(ExpectOrderForm expectOrderForm) {
+
+        //获取驾驶路线
+        CalculateDrivingLineForm calculateDrivingLineForm = new CalculateDrivingLineForm();
+        BeanUtils.copyProperties(expectOrderForm, calculateDrivingLineForm);
+        Result<DrivingLineVo> drivingLineVoResult = mapFeignClient.calculateDrivingLine(calculateDrivingLineForm);
+
+        if (drivingLineVoResult.getCode() != 200) {
+            throw new GuiguException(ResultCodeEnum.MAP_FAIL);
+        }
+        DrivingLineVo drivingLineVo = drivingLineVoResult.getData();
+
+        //获取订单费用
+        FeeRuleRequestForm feeRuleRequestForm = new FeeRuleRequestForm();
+        feeRuleRequestForm.setDistance(drivingLineVo.getDistance());
+        feeRuleRequestForm.setStartTime(new Date());
+        feeRuleRequestForm.setWaitMinute(0);
+        Result<FeeRuleResponseVo> feeRuleResponseVoResult = feeRuleFeignClient.calculateOrderFee(feeRuleRequestForm);
+
+        if (feeRuleResponseVoResult.getCode() != 200) {
+            throw new GuiguException(ResultCodeEnum.FEIGN_FAIL);
+        }
+        FeeRuleResponseVo feeRuleResponseVo = feeRuleResponseVoResult.getData();
+
+        // 封装Vo对象
+        ExpectOrderVo expectOrderVo = new ExpectOrderVo();
+        expectOrderVo.setDrivingLineVo(drivingLineVo);
+        expectOrderVo.setFeeRuleResponseVo(feeRuleResponseVo);
+
+        return expectOrderVo;
+    }
 }
